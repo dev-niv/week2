abcde25b66b35b16baf45347b87c210b47d584ce
dev-niv <nicknivu@gmail.com>
2026-02-22 17:57:06 +0530

    Update FenergoRecord.java

--- model/FenergoRecord.java
+++ model/FenergoRecord.java
@@ -1,10 +1,27 @@
-package com.example.datageneratorjava.model;
+package com.example.datagenerator.model;
+
+import java.time.LocalDate;
 
 public class FenergoRecord {
+
+    public int id;
     public String kycUnifiedId;
     public String customerName;
     public String customerType;
     public String countryCode;
     public String riskRating;
-    public String snapshotDate;
+    public LocalDate snapshotDate;
+
+    public FenergoRecord(int id, String kycUnifiedId, String customerName,
+                         String customerType, String countryCode,
+                         String riskRating, LocalDate snapshotDate) {
+
+        this.id = id;
+        this.kycUnifiedId = kycUnifiedId;
+        this.customerName = customerName;
+        this.customerType = customerType;
+        this.countryCode = countryCode;
+        this.riskRating = riskRating;
+        this.snapshotDate = snapshotDate;
+    }
 }
