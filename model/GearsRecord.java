5394a076df60e0b5c12ad9d7849f56cc78093c50
dev-niv <nicknivu@gmail.com>
2026-02-22 17:57:29 +0530

    Update GearsRecord.java

--- model/GearsRecord.java
+++ model/GearsRecord.java
@@ -1,9 +1,25 @@
-package com.example.datageneratorjava.model;
+package com.example.datagenerator.model;
+
+import java.time.LocalDate;
 
 public class GearsRecord {
+
+    public int id;
     public String kycUnifiedId;
     public String accountStatus;
     public int transactionCount;
-    public double totalValue;
-    public String snapshotDate;
+    public int totalValue;
+    public LocalDate snapshotDate;
+
+    public GearsRecord(int id, String kycUnifiedId,
+                       String accountStatus, int transactionCount,
+                       int totalValue, LocalDate snapshotDate) {
+
+        this.id = id;
+        this.kycUnifiedId = kycUnifiedId;
+        this.accountStatus = accountStatus;
+        this.transactionCount = transactionCount;
+        this.totalValue = totalValue;
+        this.snapshotDate = snapshotDate;
+    }
 }
